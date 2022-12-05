2f342a6595110a7f35aedd08ff19e380f0e532eb
Yevhenii Naumov <eugeniynaumov@gmail.com>
2022-12-05 20:15:47 +0200

    add base text and button to the Welcome component

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,5 +1,21 @@
+import {Typography} from "@mui/material";
+import Link from 'next/link'
+import Button from '@mui/material/Button';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
 export default function Welcome() {
     return <>
-        <div>Welcome component title</div>
+        <Typography variant="h5" component="p">
+            Welcome to the CRUD application.
+        </Typography>
+        <Typography variant="h6" component="p">
+            Go to the Users page to view the list...
+        </Typography>
+
+        <Link href="/users">
+            <Button variant="contained" endIcon={<NavigateNextIcon/>}>
+                Users page
+            </Button>
+        </Link>
     </>
 }
