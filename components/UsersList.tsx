be99437cc804b08d2e9750e4e54246f5af66c0e8
Yevhenii Naumov <eugeniynaumov@gmail.com>
2022-12-05 20:50:49 +0200

    add link to user page

--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -1,4 +1,3 @@
-import {useEffect} from "react";
 import UpdateData from "./updateData";
 import * as React from 'react';
 import Table from '@mui/material/Table';
@@ -8,18 +7,19 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import Button from '@mui/material/Button';
+import Link from 'next/link'
 
 export default function UsersList() {
     const users = UpdateData();
-    useEffect(() => {
-        console.log(users)
-    })
+
     function createData(
         id: string,
         name: string,
         username: string,
     ) {
-        return { id, name, username};
+        return {id, name, username};
     }
 
     const rows = Object.values(users).map((user: { id: string; name: string; username: string; }) => {
@@ -28,25 +28,33 @@ export default function UsersList() {
 
     return <>
         <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+            <Table sx={{minWidth: 650}} size="small" aria-label="a dense table">
                 <TableHead>
                     <TableRow>
                         <TableCell>ID</TableCell>
                         <TableCell align="right">Name</TableCell>
                         <TableCell align="right">Username</TableCell>
+                        <TableCell align="right">Select</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map((row) => (
                         <TableRow
                             key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
                         >
                             <TableCell component="th" scope="row">
                                 {row.id}
                             </TableCell>
                             <TableCell align="right">{row.name}</TableCell>
                             <TableCell align="right">{row.username}</TableCell>
+                            <TableCell align="right">
+                                <Link href="/user">
+                                    <Button variant="contained" endIcon={<NavigateNextIcon/>}>
+                                        Select
+                                    </Button>
+                                </Link>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
