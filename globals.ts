5c6c523128cf065fe017e44537f3b5559de325db
Yevhenii Naumov <eugeniynaumov@gmail.com>
2022-12-07 13:05:17 +0200

    rename proj

--- globals.ts
+++ globals.ts
@@ -1,5 +1,5 @@
 export namespace globals {
-    export const appName:string = 'GrainCapitalApp';
+    export const appName:string = 'UserEditorApp';
     export const yourName:string = 'Yevhenii Naumov';
     export const linkedinLink:string = 'https://www.linkedin.com/in/yevheniinaumov92/';
 }
